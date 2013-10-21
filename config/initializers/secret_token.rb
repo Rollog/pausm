7bce3929bf2a3d7829500111948a0f90e6de5718
Randy Lee <randyrlee@gmail.com>
2013-10-20 17:05:40 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pausm::Application.config.secret_key_base = '279f86427032a5b1d5bc7dc4ea67315218f1259f26ebaa828e9c78cafba861d008b805127a1f9f79048bec0fb4a24ca5fd2b115b511ca64f2723a86b833e6d88'
