f1a8580949e3c7fe4414d53f01238bfe4e3b7952
Randy Lee <randyrlee@gmail.com>
2013-11-20 10:25:00 -0800

    commmit againn??

--- app/controllers/profiles_controller.rb
+++
@@ -1,6 +0,0 @@
-class ProfilesController < ApplicationController
-	def show
-		@user = User.find(params[:id])
-		@profile = @user.profile.
-	end
-end
--- app/models/usersc.rb
+++
@@ -1,4 +0,0 @@
-class Usersc
-	field :client_id => 8dc37dea5700940ddb39fe6705954829
-	field :client_secret => 2e4c17ad35bc8f7a825ed73effe05e3a
-end
